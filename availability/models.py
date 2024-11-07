796d6172c13d98b4b7b6e6160a33e1a6e7895a29
jiyoon607 <6judy611@gmail.com>
2024-11-07 21:28:02 +0900

    ✨ Feat: 그룹 시간표 생성 (slot 모델 추가)

--- availability/models.py
+++ availability/models.py
@@ -12,3 +12,15 @@ class Availability(models.Model):
 
     def __str__(self):
         return f'{self.user.name}의 가능 시간 : {self.time_from} ~ {self.time_to}'
+    
+class Slot(models.Model):
+    id = models.AutoField(primary_key=True)
+    days = models.ForeignKey(Days, related_name="slots", on_delete=models.CASCADE)
+    availability_count = models.PositiveIntegerField(null=False, default=0, verbose_name='가능 인원 수')
+    time = models.TimeField(null=False, default=datetime.time(0, 0, 0), verbose_name='슬롯 시간')
+
+    def __str__(self):
+        return f'{self.days.group.name}의 슬롯 : {self.time} (인원: {self.availability_count})'
+    
+    class Meta:
+        unique_together = ('days', 'time')
