141b2e2224c7edeab85b860e3fc5d5e4f8c4c687
ParkSuhhyun <2022111511@dgu.ac.kr>
2024-11-13 23:22:17 +0900

    Feat: availability 생성 시 day,date입력 받도록 수정

--- availability/urls.py
+++ availability/urls.py
@@ -6,9 +6,9 @@ app_name = "availability"
 
 default_router = routers.SimpleRouter(trailing_slash=False)
 default_router.register("group-timetable", GroupTimetableViewSet, basename="group-timetable")
-default_router.register("availability", AvailiabilityViewSet, basename="availability")
+default_router.register("availability", AvailabilityViewSet, basename="availability")
 
 urlpatterns = [
     path("", include(default_router.urls)),
-    path('availability/<int:user_id>/', AvailiabilityViewSet.as_view({'get': 'list_user_availability'}), name='user-availability'),
+    path('availability/<int:user_id>/', AvailabilityViewSet.as_view({'get': 'list_user_availability'}), name='user-availability'),
 ]
